a63b2b70993764e6053eb14eddd419a108ce5de0
Emmanuel <williamsjenifermary@gmail.com>
2026-09-02 03:11:15 +0100

    Add service worker registration to app.js

--- app.js
+++ app.js
@@ -222,3 +222,11 @@ if ("serviceWorker" in navigator) {
       .catch(error => console.log("Service Worker error:", error));
   });
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.error("Service Worker registration failed:", err));
+  });
+}
